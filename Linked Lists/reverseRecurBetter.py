19190e35552e86c5247800d7cac202aeee61586d
Atharva Ashar <atharvavashar5@gmail.com>
2023-06-07 13:06:41 +0530

    Better Recursive Method

---
+++ Linked Lists/reverseRecurBetter.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next=None
+def takeinput():
+    inputList = [int(elem) for elem in input("Enter Linked list: ").split()]
+    head=None
+    for currData in inputList:
+        if currData== -1:
+            break 
+        newNode = Node(currData)
+        if head is None:
+            head=newNode
+        else:
+            curr=head
+            while curr.next != None:
+                curr=curr.next
+            curr.next=newNode
+    return head
+
+def printLL(head):
+    if head is None or head.next is None:
+        return head
+    else:
+        while head is not None:
+            print(str(head.value)+"->",end=" ")
+            head = head.next
+        print(None)
+    return
+
+def reverseRecurBetter(head):
+    if head is None or head.next is None:
+        return head,head
+    smallhead,tail=reverseRecurBetter(head.next)
+    tail.next = head
+    head.next = None
+    return smallhead,head
+
+
+head =takeinput()
+printLL(head)
+head,tail =reverseRecurBetter(head)
+printLL(head)

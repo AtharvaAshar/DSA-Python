4944bb215baa8b53446be8d2ae8043acb5d9ca2d
Atharva Ashar <atharvavashar5@gmail.com>
2023-06-19 16:18:59 +0530

    Bubble Sort on Linked List

---
+++ Linked Lists/bubbleSortLL.py
@@ -0,0 +1,95 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next=None
+def takeinput():
+    inputList = [int(elem) for elem in input("Enter Linked list: ").split()]
+    l=len(inputList)
+    head=None
+    for currData in inputList:
+        if currData== -1:
+            break 
+        newNode = Node(currData)
+        if head is None:
+            head=newNode
+        else:
+            curr=head
+            while curr.next != None:
+                curr=curr.next
+            curr.next=newNode
+    return head,l
+
+def printLL(head):
+    if head is None or head.next is None:
+        return head
+    else:
+        while head is not None:
+            print(str(head.value)+"->",end=" ")
+            head = head.next
+        print(None)
+    return
+def swap(head,i,j):
+    if i==j:
+        return head
+    p1=None 
+    c1=head 
+    p2=None 
+    c2=head 
+    for _ in range(i):
+        p1=c1 
+        c1=c1.next 
+    for _ in range(j):
+        p2=c2 
+        c2=c2.next 
+    if p1!=None:
+        p1.next=c2 
+    else:
+        head=c2 
+    if p2!=None:
+        p2.next=c1 
+    else:
+        head=c1 
+    temp=c1.next 
+    c1.next=c2.next 
+    c2.next=temp 
+    
+def bubbleSort(head,l):
+     
+    
+    for i in range(l):
+        curr=head
+        prev=None 
+        fwd=curr.next 
+        while fwd:
+            if curr.value > fwd.value:
+                if prev==None:
+                    prev=curr.next
+                    fwd=fwd.next
+                    prev.next= curr 
+                    curr.next=fwd
+                    head= prev 
+                else:
+                    temp=fwd 
+                    prev.next=fwd
+                    curr.next=temp.next 
+                    fwd.next=curr 
+                    prev=fwd 
+                    fwd=curr.next 
+            else:
+                prev=curr
+                curr=fwd 
+                fwd=fwd.next
+        i+=1
+
+
+
+    return head 
+            
+            
+
+
+
+head,l=takeinput()
+printLL(head)
+head=bubbleSort(head,l)
+printLL(head)

8ceeccdd15f7cfa88955ecc1720dbd77aeca08f6
Atharva Ashar <atharvavashar5@gmail.com>
2023-06-21 17:30:18 +0530

    check redundant brackets

---
+++ Stacks/checkRedundant.py
@@ -0,0 +1,19 @@
+string="(a+b)"
+stack=[]
+def checkRedundant(string):
+    for i in range(len(string)):
+        if string[i] !=")":
+            stack.append(string[i])
+        elif string[i] ==")":
+            hasOperator= False 
+            while len(stack)>0 and stack[-1]!="(":
+                stack.pop()
+                hasOperator=True 
+            if not hasOperator:
+                return True 
+            if len(stack)>0:
+                stack.pop()
+    return False
+                
+print(checkRedundant(string)) 
+    

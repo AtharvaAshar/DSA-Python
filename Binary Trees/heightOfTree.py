4280329dea7a6e7abb3953a01131b947b2171414
Atharva Ashar <atharvavashar5@gmail.com>
2023-06-28 22:13:51 +0530

    Find height of Binary Tree

---
+++ Binary Trees/heightOfTree.py
@@ -0,0 +1,43 @@
+class BTnode:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def printTreeDetailed(root):
+    if root is None:
+        return
+    print(root.data, end=":")
+    if root.left is not None:
+        print("L", root.left.data, end=",")
+    if root.right is not None:
+        print("R", root.right.data, end=" ")
+    print()
+    printTreeDetailed(root.left)
+    printTreeDetailed(root.right)
+
+
+def treeInput():
+    nodeData = int(input("Enter node data : "))
+    if nodeData == -1:
+        return None
+    node = BTnode(nodeData)
+    leftTree = treeInput()
+    rightTree = treeInput()
+    node.left = leftTree
+    node.right = rightTree
+    return node
+
+
+def height(root):
+    if root == None:
+        return 0
+    leftHeight = height(root.left)
+    rightHeight = height(root.right)
+    return max(leftHeight,rightHeight) + 1
+
+
+root = treeInput()
+printTreeDetailed(root)
+print(height(root))

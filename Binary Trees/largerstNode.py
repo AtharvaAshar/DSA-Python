5f210c419ba804c26172f54e1df6cd55cabc4fef
Atharva Ashar <atharvavashar5@gmail.com>
2023-06-28 19:21:59 +0530

    Find Largest Node

---
+++ Binary Trees/largerstNode.py
@@ -0,0 +1,43 @@
+class BTnode:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def printTreeDetailed(root):
+    if root is None:
+        return
+    print(root.data, end=":")
+    if root.left is not None:
+        print("L", root.left.data, end=",")
+    if root.right is not None:
+        print("R", root.right.data, end=" ")
+    print()
+    printTreeDetailed(root.left)
+    printTreeDetailed(root.right)
+
+
+def treeInput():
+    nodeData = int(input("Enter node data : "))
+    if nodeData == -1:
+        return None
+    node = BTnode(nodeData)
+    leftTree = treeInput()
+    rightTree = treeInput()
+    node.left = leftTree
+    node.right = rightTree
+    return node
+
+
+def largestNode(root):
+    if root == None:
+        return -999
+    leftLargest = largestNode(root.left)
+    RightLargest = largestNode(root.right)
+    return max(leftLargest,RightLargest,root.data)
+
+
+root = treeInput()
+printTreeDetailed(root)
+print(largestNode(root))

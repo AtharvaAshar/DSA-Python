db549628f44687d01b119d387738a146e4c396e8
Atharva Ashar <atharvavashar5@gmail.com>
2023-07-17 21:37:15 +0530

    Print All Nodes Between  K1 and K2

---
+++ Binary Search Trees/elementBetweenK1andK2.py
@@ -0,0 +1,109 @@
+from queue import Queue
+
+
+class BTnode:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def printTreeDetailed(root):
+    if root is None:
+        return
+    print(root.data, end=":")
+    if root.left is not None:
+        print("L", root.left.data, end=",")
+    if root.right is not None:
+        print("R", root.right.data, end=" ")
+    print()
+    printTreeDetailed(root.left)
+    printTreeDetailed(root.right)
+
+
+def printLevelwise(root):
+    q = Queue()
+    q.put(root)
+    while not (q.empty()):
+        curr = q.get()
+        print(curr.data, end=":")
+        if curr.left is not None:
+            print("L", curr.left.data, end=",")
+            q.put(curr.left)
+        if curr.right is not None:
+            print("R", curr.right.data, end=" ")
+            q.put(curr.right)
+        print()
+
+
+def treeInput():
+    q = Queue()
+    rootData = int(input("Enter root node data "))
+    if rootData == -1:
+        return None
+    root = BTnode(rootData)
+    q.put(root)
+    while not (q.empty()):
+        curr = q.get()
+        l = int(input(f"Enter left node of {curr.data} "))
+        if l != -1:
+            l = BTnode(l)
+            curr.left = l
+            q.put(l)
+        r = int(input(f"Enter right node of {curr.data} "))
+        if r != -1:
+            r = BTnode(r)
+            curr.right = r
+            q.put(r)
+    return root
+
+
+def levelOrderTraversal(root):
+    q = Queue()
+    q.put(root)
+    q.put(None)
+    print()
+    while not (q.empty()):
+        curr = q.get()
+        if curr is not None:
+            print(curr.data, end=":")
+            if curr.left is not None:
+                print("L", curr.left.data, end=",")
+                q.put(curr.left)
+            if curr.right is not None:
+                print("R", curr.right.data, end=" ")
+                q.put(curr.right)
+        else:
+            if q.empty():
+                break
+            else:
+                print()
+                q.put(None)
+
+
+# k1 is less than k2
+def elementBetweenK1andK2(root,k1,k2):
+    if root is None:
+        return
+    if root.data<k1:
+        elementBetweenK1andK2(root.right,k1,k2)
+    elif root.data>k2:
+        elementBetweenK1andK2(root.left,k1,k2)
+    else:
+        print(root.data)
+        elementBetweenK1andK2(root.left,k1,k2)
+        elementBetweenK1andK2(root.right,k1,k2)
+
+
+
+
+
+
+
+root = treeInput()
+k1 = int(input("Enter k1"))
+k2 = int(input("Enter k2"))
+
+levelOrderTraversal(root)
+print()
+elementBetweenK1andK2(root, k1,k2)

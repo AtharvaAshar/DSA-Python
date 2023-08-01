b1d235f3ccf06e427f0e72c29f005281549463fe
Atharva Ashar <atharvavashar5@gmail.com>
2023-08-01 18:06:55 +0530

    minor code change

--- Recursion Assignment/checkPalindrome.py
+++ Recursion Assignment/checkPalindrome.py
@@ -1,5 +1,5 @@
 def checkP(s,si,ei):
-    if si==ei:
+    if si==ei or ei<0:
         return True
     if s[si]!=s[ei]:
         return False
@@ -7,3 +7,4 @@ def checkP(s,si,ei):
     return checkP(s,si+1,ei-1)
 print(checkP("racecar",0,len('racecar')-1))
 print(checkP("racecr",0,len('racecr')-1))
+print(checkP(" ",0,len(' ')-1))

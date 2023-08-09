222d3e7c2b498759d0cd8e2fb314695660ff4d73
Atharva Ashar <atharvavashar5@gmail.com>
2023-08-09 21:35:13 +0530

    kruskal's algo for mst

---
+++ Graphs/kruskal.py
@@ -0,0 +1,38 @@
+class Edge:
+    def __init__(self,src,dest,wt) -> None:
+        self.src=src
+        self.dest=dest
+        self.wt=wt
+def kruskal(edges,v):
+    parent=[n for n in range(v)]
+    edges=sorted(edges,key=lambda x:x.wt)
+    c=0
+    output=[]
+    i=0
+    while c<v-1:
+        currEdge=edges[i]
+        srcParent=parent[currEdge.src-1]
+        destParent=parent[currEdge.dest-1]
+        if srcParent!=destParent:
+            output.append(currEdge) 
+            parent[srcParent]=destParent
+            c+=1
+        i+=1
+    return output
+
+
+
+v,e=(int(ele) for ele in input("Enter vertices and number of edges ").split())
+edges=[]
+for _ in range(e):
+    src,dest,wt=(int(ele) for ele in input("Enter src ,dest and wt of edge ").split())
+    edge=Edge(src,dest,wt)
+    edges.append(edge)
+
+output=kruskal(edges,v)
+print()
+cost=0
+for e in output:
+    print(e.src,e.dest,e.wt)
+    cost+=e.wt
+print(cost)
